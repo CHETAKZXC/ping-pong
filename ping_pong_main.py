01c20c66c8c4450c4bd280f9395fe02a9c35f716
CHETAKZXC <102910316+CHETAKZXC@users.noreply.github.com>
2022-04-10 16:39:15 +0300

    create class GameSprite

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -1,1 +1,13 @@
-#первый комментарий
+from pygame import*
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, hieght):
+
+        self.image = transform.scale(image.load(player_image), (wight, hieght))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
